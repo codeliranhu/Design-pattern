205780513d3cb56ea3c0a9000cd649b7054bae16
codeliranhu <62461347+codeliranhu@users.noreply.github.com>
2020-04-07 22:27:43 +0800

    Create implementation.cpp

---
+++ 代理模式/implementation.cpp
@@ -0,0 +1,20 @@
+
+#include "implementation.h"
+ 
+Implementation::Implementation()
+{}
+ 
+void Implementation::func_a()
+{
+    cout << "A is working" << endl;
+}
+ 
+void Implementation::func_b()
+{
+    cout << "B is working" << endl;
+}
+ 
+void Implementation::func_c()
+{
+    cout << "C is working" << endl;
+}
